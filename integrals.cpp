c247977e44d60bad211619949bf90219762267d9
nazar <xfghbdgh@gmail.com>
2023-03-11 00:00:57 +0200

    fixed division

--- integrals.cpp
+++ integrals.cpp
@@ -15,7 +15,7 @@
 
 std::pair<size_t, size_t> integrals::get_interval_size(size_t pts_per_interval, size_t steps_x, size_t steps_y) {
     for (size_t a = 1; a <= pts_per_interval; a++) {
-        if (pts_per_interval % a == 0 && a < steps_x) {
+        if (pts_per_interval % a == 0 && a <= steps_x) {
             auto b = pts_per_interval / a;
             if (b < steps_y) return std::make_pair(a, b);
         }
